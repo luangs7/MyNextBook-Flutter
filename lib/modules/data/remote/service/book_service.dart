d741a99e62a101a06f0fad0236ce35d89a171809
Luan G S <luangsjobs@gmail.com>
2023-07-11 00:12:20 -0300

    Feature/bugfixes (#5)
    * alguns ajustes de layout e api
    
    * alteracoes de layout
    
    * ajuste no ambiente de mock e login

--- lib/modules/data/remote/service/book_service.dart
+++ lib/modules/data/remote/service/book_service.dart
@@ -1,19 +1,11 @@
-import 'package:dio/dio.dart';
 import 'package:mynextbook/modules/data/remote/model/book_response.dart';
-import 'package:mynextbook/modules/data/remote/service/book_service_constants.dart';
-import 'package:retrofit/retrofit.dart';
-part 'book_service.g.dart';
 
-@RestApi()
 abstract class BookService {
-  factory BookService(Dio dio, {String baseUrl}) = _BookService;
-
-  @GET("volumes")
   Future<BookResponse> getBooks(
-    @Query(queryParam, encoded: true) String query,
-    @Query(languageRestrictQuery) String? language,
-    @Query(filterQuery) String? filter,
-    @Query(orderByQuery) String orderBy,
-    @Query(maxresultsQuery) int maxResults,
+    String query,
+    String? language,
+    String? filter,
+    String orderBy,
+    int maxResults,
   );
 }
